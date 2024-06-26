3b9d5fef87b60c72211e74ff2bea6ef3d639aaa0
Evan Lu <evalu802@gmail.com>
2024-06-26 18:54:19 +0800

    initialize timelapser, push to github

---
+++ screenshot_taker.py
@@ -0,0 +1,13 @@
+import time
+from tkinter import *
+from mss import mss
+
+root = Tk()
+root.title("Timelapse App")
+root.mainloop()
+
+
+# with mss() as sct:
+#     filename = sct.shot()
+
+# print(filename)
