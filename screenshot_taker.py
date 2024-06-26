1067ab62f1237fb0351b158dcf51eb6d3054ff76
Evan Lu <evalu802@gmail.com>
2024-06-26 19:07:15 +0800

    basic window functionality

--- screenshot_taker.py
+++ screenshot_taker.py
@@ -1,9 +1,17 @@
 import time
+import os
 from tkinter import *
 from mss import mss
 
 root = Tk()
-root.title("Timelapse App")
+root.title("Timelapse App") #title of the app
+root.minsize(300, 200)
+root.maxsize(300, 200)
+root.geometry("300x200+570+350") #sets starting resolution 300x200 and position x+y
+
+display_text = Label(root, text="This is an app for creating a timelapse")
+display_text.pack()  #this packs the text into the window
+
 root.mainloop()
 
 
